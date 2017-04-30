fa3c23d278668a678d304d68717bb90912befad2
Quynh Nguyen <quynhnguyen003@gmail.com>
2017-04-29 23:05:58 -0700

    Initial Commit

---
+++ SARate/SARate.h
@@ -0,0 +1,19 @@
+//
+//  SARate.h
+//  SARate
+//
+//  Created by Quynh Nguyen on 4/29/17.
+//  Copyright © 2017 SARate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SARate.
+FOUNDATION_EXPORT double SARateVersionNumber;
+
+//! Project version string for SARate.
+FOUNDATION_EXPORT const unsigned char SARateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SARate/PublicHeader.h>
+
+
